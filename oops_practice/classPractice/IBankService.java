a834dd0aa5bc464cda301d5c67134bde5ef38d81
Ayushman Yadav <ayushmanyadav80@gmail.com>
2025-09-15 12:31:44 +0530

    [Ayushman] Add . Bank Account management code added

---
+++ oops_practice/classPractice/IBankService.java
@@ -0,0 +1,11 @@
+package oops_practice.classPractice;
+
+public interface IBankService {
+
+    void createAccount();
+    void deposit();
+    void withdraw();
+    void checkBalance();
+    void displayInfo();
+    
+}
---
+++ oops_practice/classPractice/User.java
@@ -0,0 +1,28 @@
+package oops_practice.classPractice;
+
+public class User {
+    private String userName;
+    private int age;
+    public String aadhaarNumber;
+    public User(String userName, int age, String aadhaarNumber){
+        this.userName=userName;
+        this.age=age;
+        this.aadhaarNumber=aadhaarNumber;
+    }
+    public void setUserName(String userName){
+        this.userName=userName;
+    }
+    public String getUserName(){
+        return userName;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+    public int getAge(){
+        return age;
+    }
+    @Override
+    public String toString(){
+        return userName;
+    }
+}
